de147ac27fc478e3a0c8d3969dcc7994bbadea1a
unknown <enricoamato997@gmail.com>
2019-01-05 17:43:26 +0100

    stopwatch is now an object

--- js/stopwatch.js
+++ js/stopwatch.js
@@ -1,32 +1,44 @@
-//funcs stop, pause, and single start
-//text() play 'if'
-//alert on time ran out or 1000 score reached
-
 var seconds = document.querySelector("#secs");
 var secondsTime = 0;
+var stopper;
 
 
+class Stopwatch{
+  constructor(seconds, secondsTime){
+    this.seconds = seconds;
+    this.secondsTime = secondsTime;
+  }
 
-function startStopwatch(){
-  stopwatch = setInterval(incrementTime, 1000);
-  console.log(secondsTime);
-}
+  start(){
+    stopper = setInterval(this.incrementTime, 1000);
+  }
 
-function resetStopwatch(){
-  clearInterval(stopwatch);
-}
+  stop(){
+    clearInterval(stopper);
+  }
 
+  maxScoreReached(){
+    swal("You have reached the maximum score: " + score)
+  }
 
-function incrementTime(){
-  secondsTime ++;
-  console.log(secondsTime);
-  if (secondsTime < 10){
+  incrementTime(){
+    secondsTime++;
+    console.log(secondsTime);
+    if (secondsTime < 10){
       seconds.textContent = "0" + secondsTime;
-  } else seconds.textContent = secondsTime;
+    } else seconds.textContent = secondsTime;
 
-  if (secondsTime > 59){
-    swal("You scored " + score + " points!");
-    resetStopwatch();
-    gameCheck = true; //stop the game.
+    if (secondsTime > 59){
+      swal("You scored " + score + " points!");
+      stopwatch.stop();
+      gameCheck = true; //stop the game.
+    }
+
+    if (score >= 1000 && gameCheck == false){
+      maxScoreReached();
+    }
+
   }
 }
+
+var stopwatch = new Stopwatch(seconds, secondsTime);
